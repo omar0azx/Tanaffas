267e3a9bf7d3e03b6d1176d60ce7c3116475a01f
omar0azx <a3mr.azx@gmail.com>
2024-02-17 13:47:13 +0300

    Initial Commit

---
+++ Tanaffas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tanaffas
+//
+//  Created by omar alzhrani on 07/08/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
